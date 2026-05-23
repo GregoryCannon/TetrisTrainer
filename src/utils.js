894f9a5fc1f3f49995a8e6e16c9e74e22ba931e3
Greg Cannon <gregoryc@outlook.com>
2026-05-22 23:49:04 -0700

    Delete unused helper function

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,5 @@
 import { ROW, COLUMN, VACANT } from "./constants.js";
 
-// export function debugPrintBoard(board) {
-//   let boardStr = "";
-//   for (let r = 0; r < ROW; r++) {
-//     for (let c = 0; c < COLUMN; c++) {
-//       boardStr += board[r][c] == VACANT ? "_" : "0";
-//     }
-//     boardStr += "\n";
-//   }
-//   console.log(boardStr);
-// }
-
 const BOARD_COMPRESSION_SCHEME = {
   a: "00000",
   b: "00001",
